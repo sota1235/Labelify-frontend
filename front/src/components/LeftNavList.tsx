43f0e9da916787d875517975f3833c09feb03a89
sota1235 <sota1235@gmail.com>
2018-12-11 21:55:24 +0900

    modify: render list data

--- front/src/components/LeftNavList.tsx
+++ front/src/components/LeftNavList.tsx
@@ -3,16 +3,40 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { Person } from '@material-ui/icons';
+import { Person, Label, Filter } from '@material-ui/icons';
+
+interface SideBarItem {
+  icon: any, // TODO: React element
+  text: string,
+}
+
+const sideBarItems: SideBarItem[] = [
+  {
+    icon: <Person />,
+    text: 'Artists',
+  },
+  {
+    icon: <Label />,
+    text: 'Labels',
+  },
+  {
+    icon: <Filter />,
+    text: 'Filters',
+  },
+];
 
 const LeftNavList = () => (
   <List component="nav">
-    <ListItem>
-      <ListItemIcon>
-        <Person/>
-      </ListItemIcon>
-      <ListItemText primary="Artists" />
-    </ListItem>
+    {
+      sideBarItems.map((sideBarItem: SideBarItem) => (
+        <ListItem>
+          <ListItemIcon>
+            {sideBarItem.icon}
+          </ListItemIcon>
+          <ListItemText primary={sideBarItem.text} />
+        </ListItem>
+      ))
+    }
   </List>
 );
 
