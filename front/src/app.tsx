42c08bb6ae91215ecd0f24e88c4545ad5f3939a3
sota1235 <sota1235@gmail.com>
2018-12-11 15:13:45 +0900

    fix: deprecated warning

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -2,13 +2,19 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import {render} from "react-dom";
 import * as React from 'react';
 import LeftNavBar from './components/LeftNavBar';
+import MainTop from './components/MainTop';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <LeftNavBar/>
+      <MainTop/>
     </MuiThemeProvider>
   );
 }
