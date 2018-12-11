ed1a7704d9b6092f61ee829a887c29a279367396
sota1235 <sota1235@gmail.com>
2018-12-11 21:38:20 +0900

    fix: height of bottom bar

--- front/src/components/BottomBar.tsx
+++ front/src/components/BottomBar.tsx
@@ -4,12 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 const style: React.CSSProperties = {
   top: 'auto',
   bottom: 0,
+  height: 70,
   zIndex: 10000000,
 };
 
 const BottomBar = () => (
   <AppBar position="fixed" style={style}>
-    hoge
   </AppBar>
 );
 
