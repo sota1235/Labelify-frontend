b5345334eea71a8e8f869c068ad9dfcd2e0d9d59
sota1235 <sota1235@gmail.com>
2018-12-12 11:55:30 +0900

    modify: add play button

--- front/src/components/BottomBar.tsx
+++ front/src/components/BottomBar.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import PlayBoard from './PlayBoard';
 
 const style: React.CSSProperties = {
   top: 'auto',
   bottom: 0,
+  display: 'flex',
   height: 70,
   zIndex: 10000000,
+
 };
 
 const BottomBar = () => (
   <AppBar position="fixed" style={style} elevation={20}>
+    <PlayBoard/>
   </AppBar>
 );
 
